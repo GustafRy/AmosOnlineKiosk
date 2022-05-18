c6bf697dd50bcf44d1692684f4e95786bd071b6f
GustafRy <105789605+GustafRy@users.noreply.github.com>
2022-05-18 14:15:35 +0200

    Add files via upload

---
+++ index2.js
@@ -0,0 +1,37 @@
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+  }
+  var slideIndex = 0;
+  showSlides();
+  
+  function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}
+    slides[slideIndex-1].style.display = "block";
+    setTimeout(showSlides, 5000); // Change image every 2 seconds
+  }
+  
+  $.ajax({
+    "type": "GET",
+    "url": "http://www.random.org/integers/?num=1&min=1&max=6&col=1&base=10&format=plain&rnd=new",
+    "success": function( data, status, xhr ){
+         send_the_number_to_a_handler( parseInt( data ) );
+    }
+});
